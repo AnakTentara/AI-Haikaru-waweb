6e645915cf33cd57bdbcde3cf2f9bd93b3f4eeaf
AnakTentara <anaktentara25@gmail.com>
2025-11-18 12:36:14 +0700

    fix vercel

--- events/qr.js
+++ events/qr.js
@@ -1,12 +1,10 @@
-import qrcode from 'qrcode-terminal';
-
 export default {
   name: 'qr',
   once: false,
   execute(bot, qr) {
     console.log('\n📲 Pindai kode QR ini dengan WhatsApp Anda:');
     console.log('═══════════════════════════════════════\n');
-    qrcode.generate(qr, { small: true });
+    console.log('SCAN_THIS_QR: ', qr);
     console.log('\n═══════════════════════════════════════');
     console.log('⏳ Menunggu autentikasi...\n');
   }
