74754db48ae1a888adf40326dc96cb0fcfa8076f
AnakTentara <anaktentara25@gmail.com>
2025-11-18 11:50:58 +0700

    fix vercel

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 // buat UpTime xixixix, di UpTimeRobot.com
 import 'dotenv/config';
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-export function startServer() {
-	app.get("/", (req, res) => {
-		res.send("AI-Haikaru is awake and listening!");
-	});
+app.get("/", (req, res) => {
+    res.send("AI-Haikaru is awake and listening!");
+});
 
-	app.get("/status", (req, res) => {
-		res.json({
-			status: "running",
-			bot: "AI-Haikaru",
-			service: "Uptime Keeper",
-		});
-	});
+app.get("/status", (req, res) => {
+    res.json({
+        status: "running",
+        bot: "AI-Haikaru",
+        service: "Uptime Keeper"
+    });
+});
 
-	app.listen(PORT, "0.0.0.0", () => {
-		console.log(`[HTTP] Uptime Server is listening on port ${PORT}`);
-	});
-}
+export default app;
